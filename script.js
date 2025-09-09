f8275f5ecc9efc354061b16a7af46b168304182e
Adoni Cristian <32308799+adonicristiangerman@users.noreply.github.com>
2025-09-09 00:05:34 -0400

    Improved scrooling with event delegation

--- script.js
+++ script.js
@@ -40,12 +40,12 @@ btnScrollTo.addEventListener('click', function (e) {
 ///////////////////////////////////////
 // Page navigation
 
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
 
-    const id = this.getAttribute('href');
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
+  }
 });
