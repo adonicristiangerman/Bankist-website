0ac16a7f29958d35e580256b3446191ed28b1e96
Adoni Cristian <32308799+adonicristiangerman@users.noreply.github.com>
2025-09-10 23:02:08 -0400

    Made tabbed component functional

--- script.js
+++ script.js
@@ -49,3 +49,27 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Building the tabed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard Clause
+  if (!clicked) return;
+
+  // Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // Removing active tab
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active content tab
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
